457eae195bb10c50205f94200ea37ff5a283e320
Agnish Choudhury <agnish.gh@gmail.com>
2025-04-24 13:36:44 +0530

    adding comments

--- error_analyzer/ai_helper.py
+++ error_analyzer/ai_helper.py
@@ -1,10 +1,12 @@
 from openai import OpenAI
 import requests
 
+## If using ChatGPT, update the api_key with your openAI api key. Then change the function name from ask_chatgpt_obsolete to ask_chatgpt. Similarly, rename the
+## 2nd function below from ask_chatgpt to ask_chatgpt_obsolete if you are using Locally running LLMs
 client = OpenAI(api_key="<<your-openai-api-key-here>>")
 
-
 def ask_chatgpt_obsolete(error_info):
+    ## This function is for making requests to ChatGPT
     prompt = f"""
     I've got the following error in a program:
     ---
@@ -22,6 +24,7 @@ def ask_chatgpt_obsolete(error_info):
     return response.choices[0].message.content
 
 def ask_chatgpt(prompt: str):
+    ## This function is to make requests to the locally running LLM Model
     url = "http://localhost:11434/api/generate"
     data = {
         "model": "phi3",  # or whichever model you downloaded
